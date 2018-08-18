3b1d159bd05076173407c2428d6198dd2198d6a2
Barranco <Angel.Barranco@Emerson.com>
2018-08-18 16:51:30 -0400

    Added working filtering list.

--- src/App.js
+++ src/App.js
@@ -6,9 +6,20 @@ import HambugerMenu from 'react-hamburger-menu';
 
 class App extends Component {
   state = {
-    open: true
+    open: true,
+    locations: [
+      {title: 'Holy Hog BBQ', location: {lat: 28.0522737, lng: -82.5031171}},
+      {title: 'IHOP', location: {lat: 28.0498504, lng: -82.50315739999999}},
+      {title: 'Vizcaya Restaurante & Tapas Bar', location: {lat: 28.047907, lng: -82.50384699999999}},
+      {title: 'Carrabba\'s Italian Grill', location: {lat: 28.052608, lng: -82.5034678}},
+      {title: 'Bonefish Grill', location: {lat: 28.0694678, lng: -82.50665910000001}},
+      {title: 'Shells Seafood Restaurant Carrollwood', location: {lat: 27.9427724, lng: -82.50540889999999}}
+    ]
   }
 
+  componentDidMount() {
+
+  }
   handleClick() {
     this.setState({
       open: !this.state.open
@@ -24,7 +35,7 @@ class App extends Component {
           </div>
           <h1 className="App-title">Restaurants in Greater Carrollwood</h1>
         </header>
-        <RestaurantList open={this.state.open}/>
+        <RestaurantList open={this.state.open} locations={this.state.locations}/>
       </div>
     );
   }
--- src/RestaurantList.js
+++ src/RestaurantList.js
@@ -1,8 +1,34 @@
 import React, { Component } from 'react';
+import escapeRegExp from 'escape-string-regexp';
 
 class RestaurantList extends Component{
+    state= {
+        searchTerm: ''
+    }
+
+    filterChange(searchTerm){
+        this.setState({searchTerm})
+    }
+
     render() {
-        return <nav className={(this.props.open ? " " : "menu-hidden ") +"restaurant-list"}>Heres a list!</nav>
+        let showingLocations
+        if (this.state.searchTerm){
+            const match = new RegExp(escapeRegExp(this.state.searchTerm), 'i')
+            showingLocations = this.props.locations.filter(l => match.test(l.title));
+        }
+        else {
+            showingLocations = this.props.locations
+        }
+
+        return <nav className={(this.props.open ? " " : "menu-hidden ") +"restaurant-list"}>
+            <span className="filter-title">Location Filter</span>
+            <input type="text" className="filter" formNoValidate={true} onChange={(e) => this.filterChange(e.target.value)}/>
+            <ul className="location-list">
+            {showingLocations.map((l, i) => 
+                <li className="location-item" key={i}>{l.title}</li>
+            )}
+            </ul>
+        </nav>
     }
 }
 
